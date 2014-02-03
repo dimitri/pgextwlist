c1c2a9df2cb0af12e1710f5582dc82d1e0809c91
Christoph Berg <christoph.berg@credativ.de>
2014-02-03 15:53:28 +0100

    Fix the regression tests for 9.1

--- sql/pgextwlist.sql
+++ sql/pgextwlist.sql
@@ -3,29 +3,29 @@ SET ROLE mere_mortal;
 
 SHOW extwlist.extensions;
 
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
 
 -- pre-existing extension
 CREATE EXTENSION plpgsql;
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
 
 -- non-whitelisted extension
 CREATE EXTENSION hstore;
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
 
 -- whitelisted extensions
 CREATE EXTENSION citext;
 CREATE EXTENSION pg_trgm;
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
 
 -- whitelisted extension, but dependency is missing
 CREATE EXTENSION earthdistance;
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
 
 -- drop whitelisted extension
 DROP EXTENSION pg_trgm;
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
 
 -- drop non-whitelisted extension
 DROP EXTENSION plpgsql;
-SELECT extname, extversion FROM pg_extension ORDER BY 1;
+SELECT extname FROM pg_extension ORDER BY 1;
