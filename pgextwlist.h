5e95c99f12bfcff3c4c2b2ff8b1601c3bfb88adf
Christoph Berg <myon@debian.org>
2014-06-16 18:28:13 +0200

    Relax the version check to only reject versions < 9.1
    We have regression tests, let them handle it

--- pgextwlist.h
+++ pgextwlist.h
@@ -14,10 +14,7 @@
 #error "Unknown PostgreSQL version"
 #endif
 
-#if PG_MAJOR_VERSION != 901    \
-	&& PG_MAJOR_VERSION != 902 \
-	&& PG_MAJOR_VERSION != 903 \
-	&& PG_MAJOR_VERSION != 904
+#if PG_MAJOR_VERSION < 901
 #error "Unsupported postgresql version"
 #endif
 
