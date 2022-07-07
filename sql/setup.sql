1a395fb931a5a4f2ef9bb2b20a469d1dd39bece7
Christoph Berg <myon@debian.org>
2022-07-07 11:29:35 +0200

    Use session_preload_libraries for testing
    local_preload_libraries would only work in the Debian build.

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,5 @@
 LOAD 'pgextwlist';
-ALTER SYSTEM SET local_preload_libraries='pgextwlist';
+ALTER SYSTEM SET session_preload_libraries='pgextwlist';
 ALTER SYSTEM SET extwlist.extensions='citext,earthdistance,pg_trgm,pg_stat_statements';
 \set testdir `pwd` '/test-scripts'
 ALTER SYSTEM SET extwlist.custom_path=:'testdir';
