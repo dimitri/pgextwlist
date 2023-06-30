4f7ef7210db4c82f36dc2af999d92b82b339ed02
Christoph Berg <myon@debian.org>
2023-06-30 22:32:19 +0200

    Fix hooks test for extra DEBUG output line on PG16
    DEBUG:  executing extension script for "refint" version '1.0'

--- sql/hooks.sql
+++ sql/hooks.sql
@@ -1,3 +1,8 @@
+select case
+  when setting::int >= 160000 then 'PG 16+'
+  end as "regression output for PG version"
+from pg_settings where name = 'server_version_num';
+
 set client_min_messages = debug;
 set extwlist.custom_path = '/dummy';
 set role mere_mortal;
